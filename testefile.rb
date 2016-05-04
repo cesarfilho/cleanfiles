0f363d85f8fb5f8c6e479af709057ab306ac01e7
Cesar Roberto Pamplona Filho <cesarfilho@Mac.local>
2016-05-04 18:23:41 -0300

    adicionado recursividade para subpastas

--- testefile.rb
+++ testefile.rb
@@ -1,13 +1,24 @@
 
-folder_path = 'F:\Musicas_NEW\mp3_anderson'
-folder_path = folder_path.gsub(/\\/, '/')
-Dir.glob(folder_path + '/*').sort.each do |music|
-    if !File.directory? music
-      filename = File.basename(music, File.extname(music))
-      filename = filename.gsub(/_/, '')
-      filename = filename.gsub(/-/, ' - ')   
-      filename = filename.split.map(&:capitalize).join(' ')
-      File.rename(music, folder_path+'/' + filename + File.extname(music))      
-    end
+#folder_path = 'F:\Musicas_NEW\mp3_anderson'
+folder_path = '/Users/cesarfilho/Music'
+
+#CleanPathMusic(folder_path) 
+
+def CleanPathMusic(path)
+  path = path.gsub(/\\/, '/')
+  Dir.glob(path + '/*').sort.each do |music|
+      if File.directory? music
+        CleanPathMusic(music)
+      elsif
+        filename = File.basename(music, File.extname(music))
+        filename = filename.gsub(/_/, '').gsub(/-/, ' - ')   
+        filename = filename.split.map(&:capitalize).join(' ')
+        #if File.extname(music).uppercase == 'MP3'
+          puts filename
+        #File.rename(music, folder_path+'/' + filename + File.extname(music))  
+        #end    
+      end
+  end
 end
 
+CleanPathMusic(folder_path)
